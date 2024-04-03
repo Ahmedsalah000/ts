136c90674c9eafca335f71ac35be471c16d76eda
Ahmed Aboshendy <a7meddon123@gmail.com>
2024-04-03 23:28:52 +0200

    ts f subj

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log("hello world");
+//  tsc -h
+// Compiles the current project (tsconfig.json in the working directory.)
+// tsc app.ts util.ts
+// Ignoring tsconfig.json, compiles the specified files with default compiler options.
+// tsc -b
+// Build a composite project in the working directory.
+// tsc --init
+// Creates a tsconfig.json with the recommended settings in the working directory.
+// --watch, -w  Watch input files.
